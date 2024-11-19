98ae1b9f7f6b1f56b9f3455abc84c8ddf0dca62d
Harznadeln <harznadeln@gmail.com>
2024-11-19 14:52:12 +0200

    Fixed IP address retrieval

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+from flask import Flask, request
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Ваша IP-адреса:"
+def show_ip():
+    user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    return f"Ваша IP-адреса: {user_ip}"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
